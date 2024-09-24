0836d03a9373f6cc83eb4321f49371964516ea39
Viktor Bagrov <Keramogranitus@gmail.com>
2024-09-24 22:14:46 +0300

    Update: Add Mixins and user model

---
+++ backend/database/models/mixins/__init__.py
@@ -0,0 +1,7 @@
+from .with_datetime import ModelWithTimeMixin
+from .with_id import ModelWithIDMixin
+
+__all__ = (
+    ModelWithIDMixin,
+    ModelWithTimeMixin
+)
---
+++ backend/database/models/user.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backend.database.models.base.core import Base
+from backend.database.models.mixins import ModelWithTimeMixin, ModelWithIDMixin
+
+
+class UserModel(ModelWithIDMixin, ModelWithTimeMixin, Base):
+    __tablename__ = 'user'
+
+    login: Mapped[str] = mapped_column(nullable=False, unique=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(nullable=False, default=False)
