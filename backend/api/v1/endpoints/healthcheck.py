2500bf069560181471ed94c4a43945c9b4b9ae06
Viktor Bagrov <Keramogranitus@gmail.com>
2024-09-29 22:27:51 +0300

    Add docstring for healthcheck router

--- backend/api/v1/endpoints/healthcheck.py
+++ backend/api/v1/endpoints/healthcheck.py
@@ -13,4 +13,11 @@ healthcheck_router = APIRouter(tags=["healthcheck"])
     status_code=status.HTTP_200_OK,
 )
 async def healthcheck_endpoint() -> Mapping[str, bool]:
+    """
+    Health check for the service.
+
+        This route checks if the service is operational and accessible.
+        It returns a status code of 200 if the service is healthy, along
+        with a JSON message indicating the service status.
+    """
     return {"ok": True}
