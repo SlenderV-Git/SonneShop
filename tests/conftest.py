2e06cf4ce9c68ee553c8933370bea0cc8a6770db
Viktor Bagrov <Keramogranitus@gmail.com>
2024-10-04 10:48:50 +0300

    Update: add dependencies fixtures and test mediator for create user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+from typing import Callable
 import pytest
 import pytest_asyncio
 from faker import Faker
 from faker.providers import internet
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
+from src.database.gateway import DBGateway
 from src.database.core.connection import create_async_session_maker, create_engine
 from src.database.core.manager import TransactionManager
 from src.database.factory import create_database_factory
@@ -17,8 +20,12 @@ def faker():
 
 
 @pytest_asyncio.fixture(autouse=True, scope="session", loop_scope="session")
-async def session():
-    engine = create_engine("sqlite+aiosqlite:///example.db")
+async def engine():
+    return create_engine("sqlite+aiosqlite:///example.db")
+
+
+@pytest_asyncio.fixture(autouse=True, scope="session", loop_scope="session")
+async def session(engine: AsyncEngine):
     session = create_async_session_maker(engine)
 
     async with engine.begin() as conn:
@@ -29,9 +36,14 @@ async def session():
 
 
 @pytest_asyncio.fixture(autouse=True, scope="session", loop_scope="session")
-async def gateway(session):
+async def gateway_factory(session: AsyncSession):
     db_factory = create_database_factory(TransactionManager, session)
-    gateway = db_factory()
+    return db_factory
+
+
+@pytest_asyncio.fixture(autouse=True, scope="session", loop_scope="session")
+async def gateway(gateway_factory: Callable[[], DBGateway]):
+    gateway = gateway_factory()
 
     async with gateway:
         await gateway.manager.create_transaction()
